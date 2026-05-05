40ec400a97c3f44845657644a7151b9265cb3d31
bioroid17 <trace1997@ajou.ac.kr>
2026-05-05 23:52:59 +0900

    config and custom user

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    class MembershipChoices(models.TextChoices):
+        BASIC = ("basic", "Basic")
+        PREMIUM = ("premium", "Premium")
+
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    membership = models.CharField(
+        max_length=7,
+        choices=MembershipChoices.choices,
+        null=True,
+        blank=True,
+    )
