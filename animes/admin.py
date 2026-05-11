3790be3ea795a54cfebef5af57779091becaba03
bioroid17 <trace1997@ajou.ac.kr>
2026-05-12 01:45:57 +0900

    feat: Series 모델 추가

--- animes/admin.py
+++ animes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Anime
+from .models import Anime, Series
 
 
 @admin.register(Anime)
@@ -9,4 +9,16 @@ class AnimeAdmin(admin.ModelAdmin):
     list_display = (
         "title",
         "release_date",
+    )
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "genre",
+        "type",
+        "age_rating",
+        "is_complete",
     )
--- animes/models.py
+++ animes/models.py
@@ -3,7 +3,50 @@ from django.db import models
 from common.models import CommonModel
 
 
+class Series(CommonModel):
+
+    class Meta:
+        verbose_name_plural = "Series"
+
+    class AgeRatingChoices(models.IntegerChoices):
+        ALL = (0, "ALL")
+        R7 = (7, "7세")
+        R12 = (12, "12세")
+        R15 = (15, "15세")
+        R19 = (19, "19세")
+
+    class TypeChoices(models.TextChoices):
+        TVA = ("tva", "TVA")
+        MOVIE = ("movie", "극장판")
+        OVA = ("ova", "OVA")
+        ETC = ("etc", "기타")
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=15,
+        choices=TypeChoices.choices,
+        default=TypeChoices.TVA,
+    )
+    age_rating = models.PositiveSmallIntegerField(
+        choices=AgeRatingChoices.choices,
+        default=AgeRatingChoices.ALL,
+    )
+    is_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
 class Anime(CommonModel):
+    series = models.ForeignKey(
+        to=Series,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="animes",
+    )
     title = models.CharField(max_length=255)
     description = models.TextField()
     release_date = models.DateField()
